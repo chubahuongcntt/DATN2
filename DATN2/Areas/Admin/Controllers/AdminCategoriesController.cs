2aae63a133f65ac7a10687f7cbe5cd0971ba277d
chubahuongcntt <chubahuongltkka@gmail.com>
2023-04-15 10:27:04 +0700

    update 15/4/2023

---
+++ DATN2/Areas/Admin/Controllers/AdminCategoriesController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DATN2.Models;
+using PagedList.Core;
+using WebShop.Helpper;
+
+namespace DATN2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminCategoriesController : Controller
+    {
+        private readonly BookStore2Context _context;
+
+        public AdminCategoriesController(BookStore2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminCategories
+        public IActionResult Index(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 10;
+            var lsCategorys = _context.Categories
+                .AsNoTracking()
+                .OrderByDescending(x => x.Id);
+            PagedList<Category> models = new PagedList<Category>(lsCategorys, pageNumber, pageSize);
+
+            ViewBag.CurrentPage = pageNumber;
+            return View(models);
+        }
+
+        // GET: Admin/AdminCategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Admin/AdminCategories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,ParentId,Levels,Odering,Published,Thums,Title,Cover")] Category category, Microsoft.AspNetCore.Http.IFormFile fThums)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Name = Utilities.ToTitleCase(category.Name);
+                if (fThums != null)
+                {
+                    string extension = Path.GetExtension(fThums.FileName);
+                    string image = Utilities.SEOUrl(category.Name) + extension;
+                    category.Thums = await Utilities.UploadFile(fThums, @"category", image.ToLower());
+                }
+                if (string.IsNullOrEmpty(category.Thums)) category.Thums = "default.jpg";
+       
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Admin/AdminCategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/AdminCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,ParentId,Levels,Odering,Published,Thums,Title,Cover")] Category category, Microsoft.AspNetCore.Http.IFormFile fThums)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (fThums != null)
+                    {
+                        string extension = Path.GetExtension(fThums.FileName);
+                        string image = Utilities.SEOUrl(category.Name) + extension;
+                        category.Thums = await Utilities.UploadFile(fThums, @"category", image.ToLower());
+                    }
+                    if (string.IsNullOrEmpty(category.Thums)) category.Thums = "default.jpg";
+
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Admin/AdminCategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Admin/AdminCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'BookStore2Context.Categories'  is null.");
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+          return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ DATN2/Areas/Admin/Controllers/SearchController.cs
@@ -0,0 +1,41 @@
+﻿using DATN2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DATN2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SearchController : Controller
+    {
+        private readonly BookStore2Context _context;
+
+        public SearchController(BookStore2Context context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult FindProduce(string keyword)
+        {
+            List<Produce> ls = new List<Produce>();
+            if (string.IsNullOrEmpty(keyword) || keyword.Length < 1)
+            {
+                return PartialView("ListProducesSearchPartial", null);
+            }
+            ls = _context.Produces.AsNoTracking()
+                                  .Include(a => a.Cat)
+                                  .Where(x => x.Name.Contains(keyword))
+                                  .OrderByDescending(x => x.Name)
+                                  .Take(10)
+                                  .ToList();
+            if (ls == null)
+            {
+                return PartialView("ListProducesSearchPartial", null);
+            }
+            else
+            {
+                return PartialView("ListProducesSearchPartial", ls);
+            }
+        }
+
+    }
+}
